4d5dd4ea16ca0b78303125a34ab0a48707afa110
Григорий Рыболовлев <95378753+GrishaRybolovel@users.noreply.github.com>
2023-08-11 10:06:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from aiogram import Bot, Dispatcher, types
 
 API_TOKEN = '6638915745:AAGq0uADziYm6PB8IgsIk9_DjlIBsqHb4ik'
 
-# Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
@@ -28,17 +27,6 @@ def factorial(n):
 
     first_half = pool.apply_async(partial_factorial, args=(1, mid))
     second_half = pool.apply_async(partial_factorial, args=(mid + 1, n))
-
-    # # First thread to compute first half
-    # t1 = threading.Thread(target=partial_factorial, args=(1, mid))
-    # # Second thread to compute second half
-    # t2 = threading.Thread(target=partial_factorial, args=(mid + 1, n))
-    #
-    # t1.start()
-    # t2.start()
-    #
-    # first_half = t1.join()
-    # second_half = t2.join()
 
     return first_half.get() * second_half.get()
 
